576c1d224da609b03ba939ff3d37ecf2520ac153
unknown <maf@SEL1334.corp.local>
2014-04-08 16:51:19 +0200

    addFiles

---
+++ src/main/java/com/example/hellomaven/HelloMavenMain.java
@@ -0,0 +1,16 @@
+package com.example.hellomaven;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class HelloMavenMain {
+
+	public static void main(String[] args) {
+		
+		Logger logger = LoggerFactory.getLogger(HelloMavenMain.class);
+		logger.info("Hello Maven!");
+
+	}
+
+}
